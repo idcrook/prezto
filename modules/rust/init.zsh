cdba154b76a60d06a2f4cc22390719827973f50f
David Crook <idcrook@users.noreply.github.com>
2020-03-08 20:59:55 -0600

    update setting RUST_SRC_PATH

--- modules/rust/init.zsh
+++ modules/rust/init.zsh
@@ -10,10 +10,14 @@ fi
 # Prepend rustup cargo bin to path
 path=($HOME/.cargo/bin $path)
 
-
-# # Load Carton completion
+# FIXME: Load any completions for rust related
 # source "$HOME/.cask/etc/cask_completion.zsh" 2> /dev/null
 
+# Used by racer - https://github.com/racer-rust/racer#configuration
 if  [[ -d /usr/local/src/rust/src ]] ; then
   export RUST_SRC_PATH=/usr/local/src/rust/src
+fi
+
+if  [[ -d "$(rustc --print sysroot)/lib/rustlib/src/rust/src" ]] ; then
+  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 fi
